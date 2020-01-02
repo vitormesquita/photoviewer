738c3e5ed09b8c77e123ad465dc721dd869c4458
Vitor Mesquita <vitor.mesquita09@gmail.com>
2020-01-02 18:51:17 -0300

    removing old code and organizing response enum

--- PhotoViewer/domain/interactor/BaseInteractor.swift
+++ PhotoViewer/domain/interactor/BaseInteractor.swift
@@ -8,40 +8,6 @@
 
 import Foundation
 
-public enum Response<T> {
-   case new
-   case loading
-   case failure(Error)
-   case success(T)
-   
-   var isSuccess: Bool {
-      switch self {
-      case .success:
-         return true
-      default:
-         return false
-      }
-   }
-   
-   var isError: Bool {
-      switch self {
-      case .failure:
-         return true
-      default:
-         return false
-      }
-   }
-   
-   var isLoading: Bool {
-      switch self {
-      case .loading:
-         return true
-      default:
-         return false
-      }
-   }
-}
-
 class BaseInteractor: NSObject {
 
 }
---
+++ PhotoViewer/domain/utils/Response.swift
@@ -0,0 +1,43 @@
+//
+//  RequestResponse.swift
+//  PhotoViewer
+//
+//  Created by Vitor Mesquita on 29/11/18.
+//  Copyright © 2018 Vitor Mesquita. All rights reserved.
+//
+
+import Foundation
+
+public enum Response<T> {
+   case new
+   case loading
+   case failure(Error)
+   case success(T)
+   
+   var isSuccess: Bool {
+      switch self {
+      case .success:
+         return true
+      default:
+         return false
+      }
+   }
+   
+   var isError: Bool {
+      switch self {
+      case .failure:
+         return true
+      default:
+         return false
+      }
+   }
+   
+   var isLoading: Bool {
+      switch self {
+      case .loading:
+         return true
+      default:
+         return false
+      }
+   }
+}
