f7bff8fef36340f8fd78c02b7e2301db9e3fbc09
Vitor Mesquita <vitor.mesquita09@gmail.com>
2020-01-07 11:27:07 -0300

    created user model tests

--- PhotoViewerTests/domain/models/UserSpec.swift
+++ PhotoViewerTests/domain/models/UserSpec.swift
@@ -14,5 +14,49 @@ class UserSpec: QuickSpec {
 
    override func spec() {
       
+      describe("user model") {
+         
+         it("mapping success") {
+            //Arrange
+            let userAPI = UserAPI()
+            userAPI.id = "123"
+            userAPI.userName = "username"
+            userAPI.name = "User Name"
+            //Act
+            let user = User.map(userAPI: userAPI)
+            //Assert
+            expect(user).toNot(beNil())
+            expect(user?.id).to(equal(userAPI.id))
+            expect(user?.userName).to(equal(userAPI.userName))
+            expect(user?.name).to(equal(userAPI.name))
+         }
+         
+         it("mapping without required attrs") {
+            //Arrange
+            let userAPI = UserAPI()
+            //Act
+            let user = User.map(userAPI: userAPI)
+            //Assert
+            expect(user).to(beNil())
+         }
+         
+         context("total photo description") {
+            var user: User!
+            
+            beforeEach {
+               user = User(id: 0)
+            }
+            
+            it("when has 1 photo") {
+               user.totalPhotos = 1
+               expect(user.totalPhotosDescription).to(equal("1 photo"))
+            }
+            
+            it("when has than 1 photo") {
+               user.totalPhotos = 100
+               expect(user.totalPhotosDescription).to(equal("100 photos"))
+            }
+         }
+      }
    }
 }
